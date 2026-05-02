c20adec375e536afb10ffce46cba1e10b7af7dbc
dapson9898 <83287316+dapson9898@users.noreply.github.com>
2026-05-02 09:54:30 +0100

    Images update

--- src/pages/BookSession.jsx
+++ src/pages/BookSession.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { CheckCircle, Phone, Mail, Clock, MapPin } from 'lucide-react'
 import './BookSession.css'
 
-const FORMSPREE_ENDPOINT = 'https://formspree.io/f/mnjwnzlr'
+const FORMSPREE_ENDPOINT = 'https://formspree.io/f/maqvnvoo'
 
 const TIMES = ['9:00 AM','10:00 AM','11:00 AM','12:00 PM','1:00 PM','2:00 PM','3:00 PM','4:00 PM']
 
